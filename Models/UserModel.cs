6c5d7f6d5bb442d5615cf15243ec6f06ee408f7e
MatheusGomes <matheus2111gomes@gmail.com>
2024-09-06 13:33:53 -0300

    WorkInProgress - Start Auth Feature

---
+++ Models/UserModel.cs
@@ -0,0 +1,8 @@
+﻿public class UserModel
+{
+    public Guid Id { get; set; }
+    public string? Username { get; set; } // CRM to doctors / Document to Patients
+    public string? PasswordHash { get; set; }
+    public string? Role { get; set; } 
+    public Guid? ForeignId { get; set; } // PatientId / DoctorId
+}
