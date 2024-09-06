e99ea610b7fd66f3be2c643a78e2fdb9147128dd
MatheusGomes <matheus2111gomes@gmail.com>
2024-09-06 08:07:55 -0300

    init Appointment crud

--- Models/DoctorModel.cs
+++ Models/DoctorModel.cs
@@ -2,10 +2,10 @@
 {
     public class DoctorModel
     {
-        public int DoctorId { get; set; }
+        public Guid DoctorId { get; set; }
         public string? DoctorCRM { get; set; }
         public string? DoctorName { get; set; }
-
         public string? DoctorSpecialty { get; set; }
+        public Guid[]? AppointmentList { get; set; }
     }
 }
